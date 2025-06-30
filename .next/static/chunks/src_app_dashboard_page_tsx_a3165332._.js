87afceb13d15b1f49283e98b93afdba915cfe981
zakkivrz <joneszak526@gmail.com>
2025-06-30 21:03:18 +0100

    Add .next/static/chunks/src_app_dashboard_page_tsx_a3165332._.js 1751313798056

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a3165332._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_85c8c5a0._.js",
+  "static/chunks/src_647ed9b3._.js"
+],
+    source: "dynamic"
+});
